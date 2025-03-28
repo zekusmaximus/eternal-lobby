182145de48e750496f7f8b8a2ab2a9013e40e511
Jeffrey Zyjeski <jeff@zyjeski.com>
2025-03-28 12:40:16 -0400

    minor fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+
+// frontend/vite.config.js
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'; // Make sure sveltekit plugin is included
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()], // Keep other plugins like sveltekit()
+
+  // Add other existing config options if you have them
 });
